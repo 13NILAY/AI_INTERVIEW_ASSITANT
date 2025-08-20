ac53615a250ff71dbf5c0328cf47d5a1d6ab7a21
13NILAY <nilayrathod1303@gmail.com>
2025-08-20 19:42:10 +0530

    cors updated

--- Server/Node/config/allowedOrigins.js
+++ Server/Node/config/allowedOrigins.js
@@ -1,8 +1,9 @@
 const allowedOrigins =[
     "http://localhost:5173",
-    
+    "https://ai-interview-assitant.vercel.app",
     "http://localhost:5174",
     "http://localhost:5175",
+    "https://nilay1303-ai-interview-assistant.hf.space",
     "http://127.0.0.1",
     "http://localhost:3000",
     
